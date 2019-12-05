da08391fec7a9607f078542ffcc8577ccf8e2056
wayne.yu <wayne.yu@paytm.com>
2019-12-04 22:37:24 -0500

    Fix fromString

--- src/main/kotlin/kakuro/Board.kt
+++ src/main/kotlin/kakuro/Board.kt
@@ -1,13 +1,38 @@
 package kakuro
 
-data class Board(val xSize: Int, val ySize: Int, val sums: List<NSum>, val squares: List<Square>) {
+data class Board(val xSize: Int, val ySize: Int, val sums: List<NSum>) {
+
+    fun isSolved(): Boolean {TODO("")}
 
     fun printBoard(): String {
-        val board = (1..xSize).map{ (1..ySize).map{"0"}.toMutableList() }.toMutableList()
+        val board = (1..xSize).map{ (1..ySize).map{"00\\00"}.toMutableList() }.toMutableList()
 
-        squares.forEach { it -> board[it.x][it.y] = it.value.toString() }
+        fun pad(i: Int, filler: Char, len: Int): String {
+            val s = i.toString()
+            return CharArray(len - s.length){filler}.joinToString("") + s
+        }
+        fun spad(i: Int, len: Int): String = pad(i, ' ', len)
+        fun zpad(i: Int, len: Int): String = pad(i, '0', len)
 
-        return board.joinToString("\n"){ it -> it.joinToString("", "/", "/").replace("0", " ")}
+        sums.forEach { sum ->
+            println(sum)
+            (1..sum.length).forEach { i ->
+                println("i: $i")
+                when (sum.direction) {
+                    1 -> {
+                        board[sum.x][sum.y] = board[sum.x][sum.y].replaceRange(3, 5, spad(sum.sum, 2));
+                        board[sum.x][sum.y + i] = spad(sum.combo[i - 1], 5)
+                    }
+                    0 -> {
+                        board[sum.x][sum.y] = board[sum.x][sum.y].replaceRange(0, 2, spad(sum.sum, 2))
+                        board[sum.x + i][sum.y] = spad(sum.combo[i - 1], 5)
+                    }
+                    else -> throw Exception("direction should be 0 or 1")
+                }
+            }
+        }
+
+        return board.joinToString("\n"){ it -> it.joinToString("  ", "/", "/")}
     }
 
 
@@ -16,10 +41,58 @@ data class Board(val xSize: Int, val ySize: Int, val sums: List<NSum>, val squar
     fun neighbors(): List<Board> { TODO("") }
 
     companion object {
-        fun fromString(layout: String): Board { TODO("") }
+
+        private const val emptySpaceToken = "0"
+
+        fun fromString(layout: String): Board {
+            val xys = layout.split("\n").map{it.trimEnd().trimStart('/').trimEnd('/').toCharArray().toList().map{c -> c.toString()}}
+            val xSize = xys.size
+            val ySize = xys[0].size
+
+//            val squares = xys.mapIndexedNotNull { x, row ->
+//                row.mapIndexedNotNull { y, c ->
+//                    if (c != emptySpaceToken) c.toIntOrNull()?.let{ value -> Square(x, y, value)}
+//                    else null
+//                }
+//            }.flatten()
+
+            return Board(xSize, ySize, emptyList())
+        }
+
     }
 }
 
-data class NSum(val sum: Int, val direction: Int, val length: Int)
+data class NSum(val x: Int,
+                val y: Int,
+                val sum: Int,
+                val direction: Int,
+                val length: Int,
+                val combo: Array<Int> = arrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0)) {
 
-data class Square(val x: Int, val y: Int, val value: Int)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as NSum
+
+        if (x != other.x) return false
+        if (y != other.y) return false
+        if (sum != other.sum) return false
+        if (direction != other.direction) return false
+        if (length != other.length) return false
+        if (!combo.contentEquals(other.combo)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = x.hashCode()
+        result = 31 * result + y.hashCode()
+        result = 31 * result + sum
+        result = 31 * result + direction
+        result = 31 * result + length
+        result = 31 * result + combo.contentHashCode()
+        return result
+    }
+}
--- src/test/kotlin/kakuro/BoardSpec.kt
+++ src/test/kotlin/kakuro/BoardSpec.kt
@@ -14,21 +14,46 @@ import kotlin.test.assertTrue
 class BoardSpec : Spek({
 
     describe("board") {
+
         it("should print board") {
-            val board = Board(4, 4, emptyList(),
-                listOf(Square(0, 1, 3), Square(0, 2, 4), Square(3, 2, 5)))
+            val board = Board(5, 5,
+                listOf(
+                    NSum(0, 3, 12, 0, 3, arrayOf(2, 3, 7)),
+                    NSum(1, 1, 6, 0, 3, arrayOf(1, 2, 3)),
+                    NSum(1, 1, 7, 1, 3, arrayOf(1, 2, 4)),
+                    NSum(3, 0, 14, 1, 3, arrayOf(2, 5, 7)))
+            )
 
             val expected = """
-                / 34 /
-                /    /
-                /    /
-                /  5 /
+                /00\00  00\00  00\00  12\00  00\00/
+                /00\00   6\ 7      1      2      4/
+                /00\00      1  00\00      3  00\00/
+                /00\14      2      5      7  00\00/
+                /00\00      3  00\00  00\00  00\00/
             """.trimIndent()
 
             assertEquals(expected, board.printBoard())
         }
 
+        it("should create board from layout") {
+            val layout = """
+                /00\00  00\00  00\00  12\00  00\00/
+                /00\00   6\ 7      1      2      4/
+                /00\00      1  00\00      3  00\00/
+                /00\14      2      5      7  00\00/
+                /00\00      3  00\00  00\00  00\00/
+            """.trimIndent()
 
+            val expected = Board(4, 4,
+                    listOf(
+                            NSum(0, 3, 12, 0, 3, arrayOf(2, 3, 7)),
+                            NSum(1, 1, 6, 0, 3, arrayOf(1, 2, 3)),
+                            NSum(1, 1, 7, 1, 3, arrayOf(1, 2, 3)),
+                            NSum(3, 0, 14, 1, 3, arrayOf(2, 5, 7)))
+            )
+
+            assertEquals(expected, Board.fromString(layout))
+        }
     }
 
 })
