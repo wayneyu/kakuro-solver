cd0eec1165cdd5204e7279bb40542adeca232a59
wayne.yu <wayne.yu@paytm.com>
2020-11-18 22:03:45 -0500

    Add parse string to board

--- src/main/kotlin/kakuro/Board.kt
+++ src/main/kotlin/kakuro/Board.kt
@@ -7,24 +7,19 @@ data class Board(val xSize: Int, val ySize: Int, val sums: List<NSum>) {
     fun printBoard(): String {
         val board = (1..xSize).map{ (1..ySize).map{"00\\00"}.toMutableList() }.toMutableList()
 
-        fun pad(i: Int, filler: Char, len: Int): String {
-            val s = i.toString()
-            return CharArray(len - s.length){filler}.joinToString("") + s
-        }
+        fun pad(i: Int, filler: Char, len: Int): String = i.toString().padStart(len, filler)
         fun spad(i: Int, len: Int): String = pad(i, ' ', len)
         fun zpad(i: Int, len: Int): String = pad(i, '0', len)
 
         sums.forEach { sum ->
-            println(sum)
             (1..sum.length).forEach { i ->
-                println("i: $i")
                 when (sum.direction) {
                     1 -> {
-                        board[sum.x][sum.y] = board[sum.x][sum.y].replaceRange(3, 5, spad(sum.sum, 2));
+                        board[sum.x][sum.y] = board[sum.x][sum.y].replaceRange(3, 5, zpad(sum.sum, 2));
                         board[sum.x][sum.y + i] = spad(sum.combo[i - 1], 5)
                     }
                     0 -> {
-                        board[sum.x][sum.y] = board[sum.x][sum.y].replaceRange(0, 2, spad(sum.sum, 2))
+                        board[sum.x][sum.y] = board[sum.x][sum.y].replaceRange(0, 2, zpad(sum.sum, 2))
                         board[sum.x + i][sum.y] = spad(sum.combo[i - 1], 5)
                     }
                     else -> throw Exception("direction should be 0 or 1")
@@ -42,21 +37,71 @@ data class Board(val xSize: Int, val ySize: Int, val sums: List<NSum>) {
 
     companion object {
 
-        private const val emptySpaceToken = "0"
+        private const val emptySpaceToken = "00\\00"
+
+        fun splitByLength(str: String, len: Int, delimiterLen: Int): List<String> {
+            val period = len + delimiterLen
+            return (0..(str.length/period)).map{i -> str.substring(i*period, i*period + len)}
+        }
+
+        fun getCombo(xys: List<List<String>>, x: Int, y: Int, direction: Int): List<Int> {
+            // x, y: location of sum header
+            val out = mutableListOf<Int>()
+            val lx = xys.size
+            val ly = xys[x].size
+            when (direction) {
+                1 -> {
+                    var yi = y + 1
+                    while (yi < ly && xys[x][yi] != emptySpaceToken) {
+                        out.add(xys[x][yi].trim().toInt())
+                        yi += 1
+                    }
+                }
+                0 -> {
+                    var xi = x + 1
+                    while (xi < lx && xys[xi][y] != emptySpaceToken) {
+                        out.add(xys[xi][y].trim().toInt())
+                        xi += 1
+                    }
+                }
+                else -> throw IllegalArgumentException("Direction should bo 0 or 1")
+            }
+            return out.filter { it -> it != 0 }
+        }
 
         fun fromString(layout: String): Board {
-            val xys = layout.split("\n").map{it.trimEnd().trimStart('/').trimEnd('/').toCharArray().toList().map{c -> c.toString()}}
+            val xys = layout.split("\n").map{it.trimStart('/').trimEnd('/').let{it -> splitByLength(it, 5, 2)}}
             val xSize = xys.size
             val ySize = xys[0].size
+            val headerRegex = """(\d\d)\\(\d\d)""".toRegex()
 
-//            val squares = xys.mapIndexedNotNull { x, row ->
-//                row.mapIndexedNotNull { y, c ->
-//                    if (c != emptySpaceToken) c.toIntOrNull()?.let{ value -> Square(x, y, value)}
-//                    else null
-//                }
-//            }.flatten()
+            val sums = xys.mapIndexedNotNull { x, row ->
+                row.mapIndexedNotNull { y, str ->
+                    when {
+                        str.contains("\\") -> {
+                            val matchResult = headerRegex.find(str)
+                            val downSum = matchResult!!.groupValues[1].toInt()
+                            val rightSum = matchResult.groupValues[2].toInt()
+                            val sums = mutableListOf<NSum>()
 
-            return Board(xSize, ySize, emptyList())
+                            if (downSum > 0) {
+                                val combo = getCombo(xys, x, y, 0)
+                                val len = combo.count { it > 0 }
+                                sums.add(NSum(x, y, downSum, 0, len, combo))
+                            }
+                            if (rightSum > 0) {
+                                val combo = getCombo(xys, x, y, 1)
+                                val len = combo.count { it > 0 }
+                                sums.add(NSum(x, y, rightSum, 1, len, combo))
+                            }
+                            sums
+                        }
+                        else -> null
+                    }
+                }.flatten()
+            }.flatten()
+            println("sums $sums")
+            return Board(xSize, ySize, sums)
         }
 
     }
@@ -67,7 +112,7 @@ data class NSum(val x: Int,
                 val sum: Int,
                 val direction: Int,
                 val length: Int,
-                val combo: Array<Int> = arrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0)) {
+                val combo: List<Int> = mutableListOf(0, 0, 0, 0, 0, 0, 0, 0, 0)) {
 
 
     override fun equals(other: Any?): Boolean {
@@ -81,7 +126,7 @@ data class NSum(val x: Int,
         if (sum != other.sum) return false
         if (direction != other.direction) return false
         if (length != other.length) return false
-        if (!combo.contentEquals(other.combo)) return false
+        if (combo != other.combo) return false
 
         return true
     }
@@ -92,7 +137,7 @@ data class NSum(val x: Int,
         result = 31 * result + sum
         result = 31 * result + direction
         result = 31 * result + length
-        result = 31 * result + combo.contentHashCode()
+        result = 31 * result + combo.hashCode()
         return result
     }
 }
--- src/test/kotlin/kakuro/BoardSpec.kt
+++ src/test/kotlin/kakuro/BoardSpec.kt
@@ -18,15 +18,15 @@ class BoardSpec : Spek({
         it("should print board") {
             val board = Board(5, 5,
                 listOf(
-                    NSum(0, 3, 12, 0, 3, arrayOf(2, 3, 7)),
-                    NSum(1, 1, 6, 0, 3, arrayOf(1, 2, 3)),
-                    NSum(1, 1, 7, 1, 3, arrayOf(1, 2, 4)),
-                    NSum(3, 0, 14, 1, 3, arrayOf(2, 5, 7)))
+                    NSum(0, 3, 12, 0, 3, listOf(2, 3, 7)),
+                    NSum(1, 1, 6, 0, 3, listOf(1, 2, 3)),
+                    NSum(1, 1, 7, 1, 3, listOf(1, 2, 4)),
+                    NSum(3, 0, 14, 1, 3, listOf(2, 5, 7)))
             )
 
             val expected = """
                 /00\00  00\00  00\00  12\00  00\00/
-                /00\00   6\ 7      1      2      4/
+                /00\00  06\07      1      2      4/
                 /00\00      1  00\00      3  00\00/
                 /00\14      2      5      7  00\00/
                 /00\00      3  00\00  00\00  00\00/
@@ -38,19 +38,22 @@ class BoardSpec : Spek({
         it("should create board from layout") {
             val layout = """
                 /00\00  00\00  00\00  12\00  00\00/
-                /00\00   6\ 7      1      2      4/
+                /00\00  06\07      1      2      4/
                 /00\00      1  00\00      3  00\00/
                 /00\14      2      5      7  00\00/
                 /00\00      3  00\00  00\00  00\00/
             """.trimIndent()
 
-            val expected = Board(4, 4,
+            val expected = Board(5, 5,
                     listOf(
-                            NSum(0, 3, 12, 0, 3, arrayOf(2, 3, 7)),
-                            NSum(1, 1, 6, 0, 3, arrayOf(1, 2, 3)),
-                            NSum(1, 1, 7, 1, 3, arrayOf(1, 2, 3)),
-                            NSum(3, 0, 14, 1, 3, arrayOf(2, 5, 7)))
+                            NSum(0, 3, 12, 0, 3, listOf(2, 3, 7)),
+                            NSum(1, 1, 6, 0, 3, listOf(1, 2, 3)),
+                            NSum(1, 1, 7, 1, 3, listOf(1, 2, 4)),
+                            NSum(3, 0, 14, 1, 3, listOf(2, 5, 7)))
             )
+
+            println(expected.printBoard())
+            println(Board.fromString(layout).printBoard())
 
             assertEquals(expected, Board.fromString(layout))
         }
